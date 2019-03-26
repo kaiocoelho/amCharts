d11c1d1df74bae1d68cdf3fe97b854f60891a360
Kaio Coelho <kaio.coelho@avantsec.com.br>
2019-03-26 15:31:17 -0300

    Reposicionando os gráficos

--- js/graficos.js
+++ js/graficos.js
@@ -21,65 +21,13 @@ function Gerar() {
     }
 }
 
-function Pie() {
-    var legenda = document.getElementById("legendaidPIE").checked;
-
-    var tipoGrafico = document.getElementById("tipoGraficoPIE").value;
-    
-    // Escolhendo o tipo de gráfico a ser exibido
-    switch(tipoGrafico){
-        case '2D':
-            // Cria um instancia 2D
-            var chart = am4core.create("conteudo", am4charts.PieChart);
-            
-            // Cria a serie pie 2D
-            var series = chart.series.push(new am4charts.PieSeries());
-            series.dataFields.value = "litres";
-            series.dataFields.category = "country";
-            break;
-        case '3D':
-            // Cria um instancia 3D
-            var chart = am4core.create("conteudo", am4charts.PieChart3D);
-            
-            // Cria a serie pie 3D
-            var series = chart.series.push(new am4charts.PieSeries3D());
-            series.dataFields.value = "litres";
-            series.dataFields.category = "country";
-            break;
-    }
-    
-    // Adiciona dados
-    chart.dataSource.url = "json/data_pie.json";
-    
-    // Inserindo raio interno no gráfico
-    var raio = document.getElementById("raioidPIE").value;
-    chart.innerRadius = am4core.percent(raio);
-    
-    var label = document.getElementById("labelidPIE").checked;
-    // Habilitando/Desabilitando labels
-    series.labels.template.disabled = !label;
-    series.ticks.template.disabled = !label;
-
-    
-    // Inserindo título no grafico
-    var title = chart.titles.create();
-    var titulo = document.getElementById("tituloidPIE").value;
-    title.text = titulo;
-    title.fontSize = 25;
-    title.marginBottom = 30;
-
-    if (legenda === true) {
-        chart.legend = new am4charts.Legend();
-    }
-}
-
 function XY() {
     var cursor = document.getElementById("cursoridXY").checked;
     var legenda = document.getElementById("legendaidXY").checked;
     var zoom = document.getElementById("zoomidXY").checked;
 
     // Criando uma instancia
-    var chart = am4core.create("conteudo", am4charts.XYChart);
+    var chart = am4core.create("Q1", am4charts.XYChart);
 
     // Adicionando dados
     chart.dataSource.url = "json/data_xy.json";
@@ -203,13 +151,65 @@ function XY() {
     }
 }
 
+function Pie() {
+    var legenda = document.getElementById("legendaidPIE").checked;
+
+    var tipoGrafico = document.getElementById("tipoGraficoPIE").value;
+    
+    // Escolhendo o tipo de gráfico a ser exibido
+    switch(tipoGrafico){
+        case '2D':
+            // Cria um instancia 2D
+            var chart = am4core.create("Q2", am4charts.PieChart);
+            
+            // Cria a serie pie 2D
+            var series = chart.series.push(new am4charts.PieSeries());
+            series.dataFields.value = "litres";
+            series.dataFields.category = "country";
+            break;
+        case '3D':
+            // Cria um instancia 3D
+            var chart = am4core.create("conteudo", am4charts.PieChart3D);
+            
+            // Cria a serie pie 3D
+            var series = chart.series.push(new am4charts.PieSeries3D());
+            series.dataFields.value = "litres";
+            series.dataFields.category = "country";
+            break;
+    }
+    
+    // Adiciona dados
+    chart.dataSource.url = "json/data_pie.json";
+    
+    // Inserindo raio interno no gráfico
+    var raio = document.getElementById("raioidPIE").value;
+    chart.innerRadius = am4core.percent(raio);
+    
+    var label = document.getElementById("labelidPIE").checked;
+    // Habilitando/Desabilitando labels
+    series.labels.template.disabled = !label;
+    series.ticks.template.disabled = !label;
+
+    
+    // Inserindo título no grafico
+    var title = chart.titles.create();
+    var titulo = document.getElementById("tituloidPIE").value;
+    title.text = titulo;
+    title.fontSize = 25;
+    title.marginBottom = 30;
+
+    if (legenda === true) {
+        chart.legend = new am4charts.Legend();
+    }
+}
+
 function Radar() {
     var cursor = document.getElementById("cursoridRAD").checked;
     var legenda = document.getElementById("legendaid").checked;
     var zoom = document.getElementById("zoomidRAD").checked;
 
     // Cria uma instancia
-    var chart = am4core.create("conteudo", am4charts.RadarChart);
+    var chart = am4core.create("Q3", am4charts.RadarChart);
 
     // Adiciona dados
     chart.dataSource.url = "json/data_radar.json";
@@ -267,7 +267,7 @@ function Radar() {
 function Sliced() {
     var legenda = document.getElementById("legendaidSLI").checked;
 
-    var chart = am4core.create("conteudo", am4charts.SlicedChart);
+    var chart = am4core.create("Q4", am4charts.SlicedChart);
     chart.dataSource.url = 'json/data_sliced.json';
 
     var tipoGrafico = document.getElementById("tipoGraficoidSLI").value;
